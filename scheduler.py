9340dfddd990f0a6c8deee51f5aca32a8776be92
amandamt1224 <amandamt1224@gmail.com>
2015-08-14 09:02:21 -0700

    final updates 8-14

--- scheduler.py
+++ scheduler.py
@@ -8,7 +8,7 @@ import sys
 #load arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, type=str, help="Path to image")
-ap.add_argument("-c", "--batchSize", type=int, help="Set batch size", default=4000)
+ap.add_argument("-b", "--batchSize", type=int, help="Set batch size", default=4000)
 ap.add_argument("-p", "--processes", required=True, type=int, help="Set number of processes")
 ap.add_argument("-s", "--stepSize", type=int, help="Set frame step size", default=8)
 ap.add_argument("-t", "--threshold", type=float, help="Set prediction threshold (example: .80)", default=.80)
@@ -27,13 +27,13 @@ width = image.shape[1]
 xFrames = int(width/args.stepSize) - (int(64/args.stepSize)-1) #(width/step size) - (frameSize/step size - 1)
 yFrames = int(height/args.stepSize) - (int(64/args.stepSize)-1) #(height/step size) - (frameSize/step size - 1) 
 numFrames = xFrames * yFrames
-cSize = int(args.batchSize)
+bSize = int(args.batchSize)
 #predictions = np.empty([numFrames, 2])
 coordsArr = makeCoords(64, 64, args.stepSize, image)
 
 
 #this only runs if your batch size is greater than or equal to your total number of frames
-if len(coordsArr) <= cSize:
+if len(coordsArr) <= bSize:
 	#load caffe files and initialize Classifier
 	net = caffe.Classifier(PROTO, MODEL, mean=MEAN, image_dims=(64,64),raw_scale=255)
 	caffe.set_mode_gpu() #USES GPU BY DEFAULT
@@ -60,7 +60,7 @@ for i in range(num_processes):
 	worker.daemon = True
 	worker.start()
 
-indices = makeIndices(cSize, numFrames)
+indices = makeIndices(bSize, numFrames)
 #put tasks on queue
 for j in range(0, len(indices)):
 	q.put(indices[j])
